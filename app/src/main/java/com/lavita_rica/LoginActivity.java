b5915cd002825c5dfd488800d8a5b0a106b0d4a9
James <jordunjames@gmail.com>
2017-08-14 17:15:54 +0100

    Worked on dashboard

--- app/src/main/java/com/lavita_rica/LoginActivity.java
+++ app/src/main/java/com/lavita_rica/LoginActivity.java
@@ -1,17 +1,40 @@
 package com.lavita_rica;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    TextView signup;
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        ActionBar actionBar =  getSupportActionBar();
-        actionBar.hide();
+       // ActionBar actionBar =  getSupportActionBar();
+       // actionBar.hide();
+        login = (Button) findViewById(R.id.login);
+        signup = (TextView) findViewById(R.id.signup);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/lavita_rica/RegisterActivity.java
+++ app/src/main/java/com/lavita_rica/RegisterActivity.java
@@ -1,10 +1,14 @@
 package com.lavita_rica;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
+    Button register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,7 +16,16 @@ public class RegisterActivity extends AppCompatActivity {
         setTitle(R.string.register);
         setContentView(R.layout.activity_register);
 
-        ActionBar actionBar =  getSupportActionBar();
-        actionBar.hide();
+        //ActionBar actionBar =  getSupportActionBar();
+       // actionBar.hide();
+
+        register = (Button) findViewById(R.id.register);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
